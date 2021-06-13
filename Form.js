495dfce73e61f6613ffd5653fa12dd64d9e0262c
coder-sanskar <74972821+coder-sanskar@users.noreply.github.com>
2021-06-13 23:46:19 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,17 @@
+class Form {
+ 
+ constructor(){
+
+  this.button1 = createButton('PLAY');
+  this.button1.position(width/2,300);
+  this.button1.style('background', 'lightgrey');
+ 
+ }
+
+
+
+
+
+
+
+}
